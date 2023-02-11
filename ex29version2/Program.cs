b42e6c13560d4ae86e6a0b17b6d7c4f45f25915b
Delyash <delyash91@mail.ru>
2023-02-11 13:18:45 -0500

    version 2

---
+++ ex29version2/Program.cs
@@ -0,0 +1,13 @@
+﻿void CreateNewArray(int  size_array)
+{
+    int []array = new int [size_array];
+    for (int i = 0; i <size_array; i++)
+    {
+        Console.WriteLine($"input element of array with index {i} ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    for (int i =0; i<size_array; i++)
+        Console.Write(array[i] + " ");
+    
+}
+CreateNewArray(8);
