0b6a36e2633fd27a916403be82f754b3c994fbde
Delyash <delyash91@mail.ru>
2023-02-11 13:12:56 -0500

    ex25

---
+++ ex25/Program.cs
@@ -0,0 +1,25 @@
+﻿int Cycle(int a, int b)
+{
+    int i = 1;
+    int newNumber = 1;
+    while ( i <= b )
+    {
+        newNumber = newNumber * a;
+        i++;
+    }
+ Console.WriteLine($"degree of element {a} is {newNumber}");
+ return newNumber;
+}
+
+Console.WriteLine("input your number a");
+int userA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input your number b");
+int userB = Convert.ToInt32(Console.ReadLine());
+
+
+if ( userB >= 0)
+{
+    Cycle(userA,userB);
+  
+}       
+else Console.WriteLine("uncorrect number");
