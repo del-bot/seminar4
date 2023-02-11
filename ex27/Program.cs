23fc805936659e8650c8904bcaa8bd6b8a66cff1
Delyash <delyash91@mail.ru>
2023-02-11 13:14:40 -0500

    ex27

---
+++ ex27/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberDivide(int number)
+{
+    int count = Convert.ToString(number).Length;
+    int memor = 0;
+    int summ = 0;
+    for ( int i = 0; i < count; i++)
+    {
+        memor = number % 10;
+        summ =  memor + summ; 
+        number/=10;
+    }
+    Console.WriteLine($"summ is {summ}");
+    return summ;   
+}
+
+Console.WriteLine("input your number");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+numberDivide(userNumber);
