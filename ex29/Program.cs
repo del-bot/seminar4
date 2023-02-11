41fa437f5d34ea2cb51a73fe5c58446d1b305765
Delyash <delyash91@mail.ru>
2023-02-11 13:17:01 -0500

    ex29 first version

---
+++ ex29/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] CreateNewArray(int size_array)
+{
+    int [] random01array = new int [size_array];
+    for (int i= 0; i < size_array; i++)
+    {
+        random01array[i] = new Random().Next(0, 40);
+    }
+    return random01array;
+}
+
+void ShowCreatedArray(int [] array01)
+{
+    for (int i = 0; i < array01.Length; i++)
+        Console.WriteLine(array01[i] + " ");
+}
+ShowCreatedArray(CreateNewArray(8));
